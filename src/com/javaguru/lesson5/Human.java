e1a298f3e2250277c6d6733e8ed8e8c3bb050a1b
java2017autumn@gmail.com <java2017autumn@gmail.com>
2017-10-22 10:45:29 +0300

    lesson 5

--- src/com/javaguru/lesson5/Human.java
+++ src/com/javaguru/lesson5/Human.java
@@ -1,4 +1,28 @@
 package com.javaguru.lesson5;
 
 public class Human {
+
+    int age;
+    String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello! My name is "
+                + getName() + ". I am " + getAge() + " years old.");
+    }
 }
---
+++ src/com/javaguru/lesson5/HumanMain.java
@@ -0,0 +1,11 @@
+package com.javaguru.lesson5;
+
+public class HumanMain {
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.setName("John");
+        human.setAge(15);
+        human.sayHello();
+    }
+}
