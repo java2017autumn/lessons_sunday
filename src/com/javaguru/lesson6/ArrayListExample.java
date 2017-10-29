e50a4efbac4c83aae1e3774055841076081ba46a
java2017autumn@gmail.com <java2017autumn@gmail.com>
2017-10-29 10:34:15 +0200

    lesson 6

---
+++ src/com/javaguru/lesson6/ArrayListExample.java
@@ -0,0 +1,31 @@
+package com.javaguru.lesson6;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+
+        while (true) {
+            String userInput = getStringFromUser();
+            if (userInput.equals("")) {
+                break;
+            }
+            strings.add(userInput);
+        }
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+    }
+
+
+    public static String getStringFromUser(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
